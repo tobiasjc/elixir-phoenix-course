d741453b20b1ee3893f8d48accbf31cc8d277c95
José Tobias <jose.tobias@outlook.com>
2020-07-10 17:39:00 -0300

    feat: Models in Phoenix
    Signed-off-by: José Tobias <jose.tobias@outlook.com>

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,7 +1,12 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
-  def new(conn, params) do
+  def new(conn, _attr) do
+    changeset = Discuss.Topic.changeset(%Discuss.Topic{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"topic" => topic}) do
 
   end
 end
